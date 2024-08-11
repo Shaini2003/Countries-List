b2c933382a7d4d3a12b92a8654b00f0015b1ed66
Shaini2003 <shinitharushika2020@gmail.com>
2024-08-11 13:49:29 +0530

    First commit

---
+++ app.js
@@ -0,0 +1,19 @@
+let tblCountries = document.getElementById("tblCountries");
+let tableBody = `<tr>
+                    <th>Name</th>
+                    <th>Flag</th>
+                 </tr>`;
+
+fetch(" https://restcountries.com/v3.1/all")
+    .then((res) => res.json())
+    .then(data => {
+
+        data.forEach(element => {
+            tableBody += `<tr>
+                              <td>${element.name.common}</td>
+                              <td>${element.flag}</td>
+                         </tr>`
+            console.log(element.name.common);
+        });
+        tblCountries.innerHTML = tableBody;
+    })
