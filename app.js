a6d4fe28a98148aaf7a264e75215dab8c9100fd8
Shaini2003 <shinitharushika2020@gmail.com>
2024-08-11 18:09:51 +0530

    second commit

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 let tblCountries = document.getElementById("tblCountries");
-let tableBody = `<tr>
-                    <th>Name</th>
-                    <th>Flag</th>
-                 </tr>`;
+let tableBody = `<thead class="table-light">
+                    <tr>
+                       <th class="text-primary">Name</th>
+                       <th class="text-primary">Flag</th>
+                    </tr>
+                </thead>`;
 
 fetch(" https://restcountries.com/v3.1/all")
     .then((res) => res.json())
@@ -10,7 +12,17 @@ fetch(" https://restcountries.com/v3.1/all")
 
         data.forEach(element => {
             tableBody += `<tr>
-                              <td>${element.name.common}</td>
+                              <td><h1>${element.name.common}</h1><br>
+
+                              Official Name : ${element.name.official}<br>
+                              Capital : ${element.capital}
+                              <p>Region : ${element.region} </p>
+                              Population : ${element.population}
+                              <p>Area : ${element.area} </p>
+                              <p>Time Zone : ${element.timezones} </p>
+                              <a class="btn btn-primary" href="${element.maps.googleMaps}">Go To Map </a>
+
+                              </td>
                               <td><img src="${element.flags.png}"alt=""></td>
                          </tr>`
             console.log(element.name.common);
